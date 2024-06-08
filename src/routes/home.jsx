285b0743204d01164cb2ffcdd134bdd4e4ea36e2
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-06-07 23:11:35 -0300

    feat: set cards style

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
         <>
             <div className="flex-col" id="layout">
                 <header className="flex-col" id="header">
-                    <Typography.Title level={1} id="page-title">
+                    <Typography.Title level={1} id="pageTitle">
                         REGISTRO DE FORNECEDORES
                     </Typography.Title>
 
@@ -41,7 +41,45 @@ export default function Home() {
                         />
                     </div>
                 </header>
-                <div>CARD</div>
+                <div id="content">
+                    <Card className="supplierCard">
+                        <span className="cardTitle">Add supplier</span>
+                    </Card>
+                    {suppliers &&
+                        suppliers.map((supplier) => {
+                            return (
+                                <Card
+                                    className="supplierCard"
+                                    key={supplier.id}
+                                >
+                                    <div className="flex-col cardBody">
+                                        <div className="cardHeader flex-col">
+                                            <span className="cardTitle ">
+                                                {supplier.name}
+                                            </span>
+                                            <span className="cardContent">
+                                                {supplier.branch}
+                                            </span>
+                                        </div>
+
+                                        <span className="cardContent">
+                                            {supplier.phone}
+                                            <br />
+                                            {supplier.email}
+                                            <br />
+                                            {supplier.website}
+                                        </span>
+
+                                        <span className="cardContent">
+                                            {supplier.streetAddress}
+                                            <br />
+                                            {supplier.location}
+                                        </span>
+                                    </div>
+                                </Card>
+                            );
+                        })}
+                </div>
             </div>
         </>
     );

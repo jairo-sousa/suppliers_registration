7ae0567e2f6289a5c4b272471cab96b2613ee64c
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-06-09 22:03:33 -0300

    feat: set errorPage style

--- src/routes/errorPage.jsx
+++ src/routes/errorPage.jsx
@@ -1,13 +1,14 @@
 import { useRouteError } from "react-router-dom";
+import "../styles/errorPage.css";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
+        <div id="errorPage" className="flex-col flex-center">
+            <h1 id="errorPageTitle">Oops!</h1>
+            <p>Desculpe, Ocorreu um erro inesperado.</p>
             <p>
                 <i>{error.statusText || error.message}</i>
             </p>
